5842d8c820d9d97bd25dcece828f5c4ce364fb23
proMahadi <professionalmahadi@gmail.com>
2025-02-21 17:57:04 +0600

    login page validated

--- src/components/Pages/Auth/Login.jsx
+++ src/components/Pages/Auth/Login.jsx
@@ -3,7 +3,7 @@ import { NavLink, useNavigate, useLocation } from "react-router-dom";
 import { useState } from "react";
 import "@/styles/Auth.css";
 import { FcGoogle } from "react-icons/fc";
-import { useForm } from "react-hook-form"
+import { useForm } from "react-hook-form";
 
 export default function Login({
   User,
@@ -12,16 +12,53 @@ export default function Login({
   setIsAuthenticated,
 }) {
   const navigate = useNavigate();
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [error, setError] = useState("");
-  const [isLoading, setIsLoading] = useState(false);
+  const {
+    register,
+    formState: { isLoading, errors, isSubmitting },
+    handleSubmit,
+    setError,
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  // const [email, setEmail] = useState("");
+  // const [password, setPassword] = useState("");
+  // const [error, setError] = useState("");
+  // const [isLoading, setIsLoading] = useState(false);
 
-  const handleLogin = async () => {
+  // const handleLogin = async () => {
+  //   if (isLoading) return;
+
+  //   setIsLoading(true);
+  //   setError("");
+
+  //   const users = JSON.parse(localStorage.getItem("users") || "[]");
+  //   const user = users.find(
+  //     (u) => u.email === email && u.password === password
+  //   );
+
+  //   // Add 1.5 second delay
+  //   await new Promise((resolve) => setTimeout(resolve, 1500));
+
+  //   if (user) {
+  //     setUser(user);
+  //     sessionStorage.setItem("user", JSON.stringify(user));
+  //     setIsAuthenticated(true);
+  //     sessionStorage.setItem("isAuthenticated", "true");
+  //     navigate("/");
+  //   } else {
+  //     setError("Invalid email or password");
+  //   }
+
+  //   setIsLoading(false);
+  // };
+  const onSubmit = async (data) => {
+    const { email, password } = data;
     if (isLoading) return;
 
-    setIsLoading(true);
-    setError("");
+    setError("root", { type: "manual", message: "" });
 
     const users = JSON.parse(localStorage.getItem("users") || "[]");
     const user = users.find(
@@ -38,10 +75,11 @@ export default function Login({
       sessionStorage.setItem("isAuthenticated", "true");
       navigate("/");
     } else {
-      setError("Invalid email or password");
+      setError("root", {
+        type: "manual",
+        message: "Invalid email or password",
+      });
     }
-
-    setIsLoading(false);
   };
 
   return (
@@ -51,43 +89,66 @@ export default function Login({
           <h1>Log in to your account</h1>
           <div>Find your dream collaboration here!</div>
         </header>
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="formInputs">
             <div>
               <Input
                 placeholder="Email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                // value={email}
+                // onChange={(e) => setEmail(e.target.value)}
                 disabled={isLoading}
                 width={"100%"}
+                {...register("email", { required: "email is required" })}
               />
+              {errors.email && (
+                <p
+                  style={{
+                    color: "red",
+                    fontSize: "12px",
+                  }}
+                >
+                  {errors.email.message}
+                </p>
+              )}
             </div>
             <div>
               <Input.Password
                 placeholder="Password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                // value={password}
+                // onChange={(e) => setPassword(e.target.value)}
                 disabled={isLoading}
                 width={"100%"}
+                {...register("password", { required: "password is required" })}
               />
+              {errors.password && (
+                <p
+                  style={{
+                    color: "red",
+                    fontSize: "12px",
+                  }}
+                >
+                  {errors.password.message}
+                </p>
+              )}
             </div>
           </div>
           <Spacer h={0.1} inline></Spacer>
-        <Button
-          type="secondary-light"
-          onClick={handleLogin}
-          loading={isLoading}
-        >
-          Log in
-        </Button>
+          <Button
+            htmlType="submit"
+            type="secondary-light"
+            // onClick={handleLogin}
+            loading={isSubmitting}
+          >
+            Log in
+          </Button>
         </form>
 
         <Divider> Or</Divider>
 
         <div className="prompt">
-          <div style={{ color: error ? "red" : "inherit" }}>
+          {/* <div style={{ color: error ? "red" : "inherit" }}>
             {error || "Not yet a member?"}
-          </div>
+          </div> */}
           <div className="prompt-btns">
             <Button
               // style={{ width: "fit-content" }}
@@ -100,7 +161,7 @@ export default function Login({
             <Button
               className="signin-google-btn"
               // style={{ width: "fit-content" }}
-              onClick={handleLogin}
+              // onClick={handleLogin}
               loading={isLoading}
               disabled={isLoading}
             >
