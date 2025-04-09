28272f7dba6010d199c7f0d18255207e316d5564
proMahadi <professionalmahadi@gmail.com>
2025-04-09 16:00:52 +0600

    saved listing toggle api integrated

--- src/api/listings.js
+++ src/api/listings.js
@@ -4,3 +4,21 @@ export async function getListings() {
   const { data } = await clientAxios.get('/account/listings/')
   return data
 }
+
+
+export async function getSavedListing() {
+  const{data}= await clientAxios.get("account/saved-listing/")
+  return data
+}
+export async function saveListing(id) {
+  const{data}= await clientAxios.post("account/saved-listing/",{
+    listing_id:id
+  })
+  return data
+}
+export async function deleteListing(id) {
+  const{data}= await clientAxios.delete("account/delete-saved-listing/",{
+    listing_id:id
+  })
+  return data
+}
